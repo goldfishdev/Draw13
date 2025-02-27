693d2c1fd210b8ed8cb508b4a917c8b3adcd1b78
goldfishdev <goldfishdeveloper@gmail.com>
2025-02-27 13:33:04 -0800

    restructured some files

--- MAIN.C
+++
@@ -1,9 +0,0 @@
-#include "d13lib.h"
-
-int main() {
-	set_vga_mode(); // starts mode 13h
-	printf("Hello World!");
-	getch(); // waits for any input before leaving
-	set_text_mode(); // leaves mode 13h
-	return 0;
-}
